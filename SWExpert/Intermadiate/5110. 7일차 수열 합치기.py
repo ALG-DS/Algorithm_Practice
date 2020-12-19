92f424b7fe84688cdf0c64879958a145c1bd5a9e
mack1210 <caggun1210@gmail.com>
2020-12-20 02:55:31 +0900

    Update 5110. 7일차 수열 합치기.py

--- SWExpert/Intermadiate/5110. 7일차 수열 합치기.py
+++ SWExpert/Intermadiate/5110. 7일차 수열 합치기.py
@@ -64,21 +64,111 @@
 #3 404 483 16 788 123 274 231 659 778 178
 '''
 
-T = 1 #int(input())
+T = int(input())
 
 for t in range(1, T+1):
-    N, M = 4, 4 #map(int, input().split())
+    # N 수열의 길이, M 수열의 갯수
+    N, M = map(int, input().split())
 
-    arr = list(map(int, input().split()))
-    temp_arr = []
-    for m in range(M-1):
-        temp_arr.append(list(map(int, input().split())))
+    # 모든 리스트들을 저장
+    lst = []
+    for m in range(M):
+        lst.append(list(map(int, input().split())))
     
-    for i in range(M):
-        if temp_arr[i][0] > arr[i]:
-            
+    # 리스트가 둘 이상 있으면 비교 진행
+    while len(lst) > 1:
+        # 비교에 성공했는지 여부를 저장하기위해 -1로 초기화 (비교 후에도 -1이면 맨 뒤로 보내서 append)
+        idx = -1
+
+        # 
+        for i in range(len(lst[0])):
+            if lst[0][i] > lst[1][0]:
+                idx = i
+                # 끼워 넣기
+                lst[0][idx:idx] = lst[1]
+                # 끼워넣고 나면 비교 리스트 삭제
+                lst.pop(1)
+                break
+        # 비교에 실패했다면 맨 뒤로 보내서 append
+        if idx == -1:
+            for j in lst[1]:
+                lst[0].append(j)
+            # 넣고 나면 비교리스트 삭제
+            lst.pop(1)
+
+    result = lst[0][:]
+    joined_result = ' '.join(map(str, result[::-1][:10]))
+
+    print("#{0} {1}".format(t, joined_result))
 
 
+'''
+# 5110. [파이썬 S/W 문제해결 기본] 7일차 - 수열 합치기
+ 
+ 
+class Node:
+    def __init__(self, d=0, p=None, n=None):
+        self.data = d
+        self.prev = p
+        self.next = n
+ 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+ 
+def addList(lst, arr):
+    first = last = Node(arr[0])
+    for val in arr[1:]:
+        new = Node(val, last)
+        last.next = new
+        last = new
+ 
+    if lst.head is None:
+        lst.head, lst.tail = first, last
+    else:
+        cur = lst.head
+        while cur is not None:
+            if cur.data > arr[0]: break
+            cur = cur.next
+        if cur is None: # 뒤에 # 순서주의
+            first.prev = lst.tail
+            lst.tail.next = first
+            lst.tail = last
+        elif cur.prev is None: # 앞에 
+            last.next = lst.head
+            lst.head.prev = last
+            lst.head = first
+        else: # 중간에 추가
+            prev = cur.prev
+            first.prev = prev
+            last.next = cur
+            prev.next = first
+            cur.prev = last
+    lst.size += len(arr)
+ 
+def printList(lst):  # 연결리스트 출력
+    if lst.head is None:
+        return
+    cur = lst.tail
+    cnt = 10
+    while cnt: # 역방향
+        print(cur.data, end=' ')
+        cur = cur.prev
+        cnt -= 1
+    print()
+ 
+# import sys
+# sys.stdin = open('input.txt')    
+ 
+for tc in range(1,int(input())+1):
+    n,m=map(int,input().split())
+    arr = list(list(map(int,input().split())) for _ in range(m)) # 2차원배열로 받기
+    mylist = LinkedList()
+    for i in range(m):
+        addList(mylist, arr[i])
+    print(f'#{tc}', end=' ')
+    printList(mylist)
 
-
-
+'''
